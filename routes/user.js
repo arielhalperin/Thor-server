2543b0598bc55d6b1b6d9221f4098d109d3ef8bd
ariel.halperin <ariel.halperin@philips.com>
2017-12-04 17:52:20 +0200

    add login user + jwt

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
+var jwt =  require('jsonwebtoken');
+
 /* GET users listing. */
 router.post('/', function(req, res, next) {
 
@@ -27,5 +29,31 @@ router.post('/', function(req, res, next) {
         });
     });
 });
+
+router.post('/login', function (req, res, next) {
+    User.findOne({email: req.body.email}, function(err, user){
+        if (err) {
+            return res.status(500).json({
+                title: 'An error Occured',
+                error: err
+            });
+        }
+
+        if(!user || (req.body.password !== user.password)){
+            return res.status(401).json({
+                title: 'Login Failed',
+                error: {message: 'Invalid login credentials'}
+            });
+        }
+
+        var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+        res.status(200).json({
+            message: 'Successfully logged in',
+            token: token,
+            userId: user._id
+        });
+    });
+});
+
 
 module.exports = router;
